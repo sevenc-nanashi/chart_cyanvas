386612d0c0c362da8e34149538542db257def96d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-01-23 21:11:30 +0900

    Fix: Fix type error

--- frontend/lib/requireLogin.tsx
+++ frontend/lib/requireLogin.tsx
@@ -3,11 +3,12 @@ import { NextPage } from "next"
 import { useRouter } from "next/router"
 import { createElement } from "react"
 
-const requireLogin = (component: NextPage) => {
-  const Inner: React.FC<Record<string, unknown>> = (props) => {
+const requireLogin = <T,>(component: NextPage<T>) => {
+  const Inner: NextPage<T> = (props) => {
     const [session] = useSession()
     const router = useRouter()
     if (session.loggedIn === undefined) return <div></div>
+    // @ts-expect-error createElement limitation?
     if (session.loggedIn) return createElement(component, props)
     router.push("/login")
     return <div></div>
