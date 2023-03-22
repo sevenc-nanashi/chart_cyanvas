9f816724b6e9679ccae6650d790d8b8ad0792a83
Nanashi <sevenc7c@sevenc7c.com>
2023-03-22 09:37:00 +0900

    Add: Add cloudflare analytics

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,6 +9,13 @@ export default class Document extends NextDocument {
           <link rel="shortcut icon" href="/favicon.svg" />
           <link rel="favicon" href="/favicon.svg" />
           <meta name="theme-color" content="#83ccd2" />
+          {/* Cloudflare Web Analytics */}
+          <script
+             defer
+             src="https://static.cloudflareinsights.com/beacon.min.js"
+             data-cf-beacon='{"token": "5d4e8a97143447a293a04ba13358db27"}'
+          />
+          {/* End Cloudflare Web Analytics */}
         </Head>
         <body>
           <Main />
