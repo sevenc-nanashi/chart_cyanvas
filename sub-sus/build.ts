2f0d27282afd284149625a78d4d893a05f9ee4cc
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-03-27 23:48:31 +0900

    Fix: Fix Filename

--- sub-sus/build.ts
+++ sub-sus/build.ts
@@ -3,7 +3,7 @@ import * as esbuild from "esbuild"
 esbuild.build({
   entryPoints: ["./src/index.ts"],
   bundle: true,
-  outfile: "dist/index.cjs",
+  outfile: "dist/index.js",
   platform: "node",
   minify: true,
 })
