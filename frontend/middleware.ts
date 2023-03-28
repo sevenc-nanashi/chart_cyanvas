c3405179c30dfb48aa66d4872c723af625fd972c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-03-28 23:38:23 +0900

    Fix: Fix ruby version specifier

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export function middleware(req: NextRequest) {
+  if (
+    ["/api", "/_next", "/favicon.ico"].some((path) =>
+      req.nextUrl.pathname.startsWith(path)
+    )
+  ) {
+    console.log("middleware", req.nextUrl.pathname)
+    return NextResponse.next()
+  }
   if (req.nextUrl.locale === "default") {
     const locale = req.cookies.get("NEXT_LOCALE")?.value || "en"
 
