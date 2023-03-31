0b9ec4473a4acadf065523965187a1285ea22d05
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-03-31 16:12:24 +0900

    Code: Format code

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_URL"]}" }
-end
+Sidekiq.configure_server { |config| config.redis = { url: ENV["REDIS_URL"] } }
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV["REDIS_URL"]}" }
-end
+Sidekiq.configure_client { |config| config.redis = { url: ENV["REDIS_URL"] } }
