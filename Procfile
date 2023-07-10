c35fe4a996d96c7093e4507718b517392695a41e
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-10 22:17:47 +0900

    Change: Use console for logger

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 frontend: cd frontend && pnpm dev
-backend: cd backend && bundle exec rails s -p 3000
+backend: cd backend && CONSOLE_OUTPUT=XTerm bundle exec rails s -p 3000
 backend-sidekiq: cd backend && bundle exec sidekiq
 sub-audio: cd sub-audio && poetry poe dev
 sub-image: cd sub-image && poetry poe dev
---
+++ backend/config/initializers/logger.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails"
+
+require "console"
+require "console/compatible/logger"
+
+if ActiveSupport::Logger.respond_to?(:logger_outputs_to?)
+  # https://github.com/rails/rails/issues/44800
+  class ActiveSupport::Logger
+    def self.logger_outputs_to?(*)
+      true
+    end
+  end
+end
+
+Rails.logger = Console::Compatible::Logger.new("Rails", Console.logger.output)
