e351d437a3b260b7651388dff75dc3f4edfceb4a
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-15 15:04:57 +0900

    Add: Add discord column

---
+++ backend/db/migrate/20230715055614_add_discord_info_to_user.rb
@@ -0,0 +1,8 @@
+class AddDiscordInfoToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :discord_id, :string
+    add_column :users, :discord_token, :string
+    add_column :users, :discord_refresh_token, :string
+    add_column :users, :discord_expires_at, :datetime
+  end
+end
