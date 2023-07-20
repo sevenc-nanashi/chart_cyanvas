aadc84e30f7cf8def23b141bc0b12a142504cc1d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-20 21:14:14 +0900

    Change: Disable discord in test

--- backend/config/initializers/discord.rb
+++ backend/config/initializers/discord.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "../../lib/discord_request"
-$discord = DiscordRequest.new(bot_token: ENV["DISCORD_BOT_TOKEN"])
+if Rails.env.test?
+  class Disabled
+    def method_missing(*_args)
+      raise "Discord disabled in test environment"
+    end
+  end
+  $discord = Disabled.new
+  # Disable
+else
+  $discord = DiscordRequest.new(bot_token: ENV["DISCORD_BOT_TOKEN"])
 
-info = $discord.get("/users/@me")
+  info = $discord.get("/users/@me")
 
-Rails.logger.info(
-  "Discord: Logged in as #{info[:username]}##{info[:discriminator]}"
-)
+  Rails.logger.info(
+    "Discord: Logged in as #{info[:username]}##{info[:discriminator]}"
+  )
+end
