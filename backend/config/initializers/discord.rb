849c235c7110399e0a38b3f4b111953d5840c672
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-20 22:44:57 +0900

    Fix: Fix error in migrate

--- backend/config/initializers/discord.rb
+++ backend/config/initializers/discord.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 require_relative "../../lib/discord_request"
-if Rails.env.test?
-  class Disabled
-    def method_missing(*_args)
-      raise "Discord disabled in test environment"
-    end
+class Disabled
+  def method_missing(*_args)
+    raise "Discord disabled"
   end
+end
+if Rails.env.test?
   $discord = Disabled.new
   # Disable
 else
   $discord = DiscordRequest.new(bot_token: ENV["DISCORD_BOT_TOKEN"])
 
-  info = $discord.get("/users/@me")
+  begin
+    info = $discord.get("/users/@me")
+  rescue StandardError
+    Rails.logger.error("Discord: Failed to log in, disabling")
+    $discord = Disabled.new
+  end
 
   Rails.logger.info(
     "Discord: Logged in as #{info[:username]}##{info[:discriminator]}"
