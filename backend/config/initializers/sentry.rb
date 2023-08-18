15f2641bc05726aab48099509a34f0e5c3adcf66
Nanashi <sevenc7c@sevenc7c.com>
2023-08-19 08:19:46 +0900

    Add: Add performance

--- backend/config/initializers/sentry.rb
+++ backend/config/initializers/sentry.rb
@@ -6,5 +6,7 @@ if ENV["SENTRY_DSN_BACKEND"].present?
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN_BACKEND"]
     config.breadcrumbs_logger = [:active_support_logger]
+
+    config.traces_sample_rate = Rails.env.production? ? 0.2 : 1.0
   end
 end
