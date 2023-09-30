646c804cc46462a14505ae66db0f9c5ff21e779f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-09-30 22:24:19 +0900

    Fix: Fix endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
 
     get "/admin", to: "api/admin#data"
     get "/admin/users/:handle", to: "api/admin#show_user"
-    post "/admin/reconvert_sus", to: "api/admin#reconvert_sus"
+    post "/admin/reconvert_chart", to: "api/admin#reconvert_chart"
     post "/admin/delete-chart", to: "api/admin#delete_chart"
 
     get "/my/discord", to: "api/discord#my_discord"
