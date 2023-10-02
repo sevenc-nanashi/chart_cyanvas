af8c418902b5fcad3ced9fa1d96bf87cf59882d1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-10-02 19:31:59 +0900

    Fix: Fix routing

--- sub-image/src/main.rs
+++ sub-image/src/main.rs
@@ -2,18 +2,25 @@ mod models;
 mod result;
 mod routes;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use std::{env, net::SocketAddr};
 use tower_http::trace::{self, TraceLayer};
 use tracing::{info, Level};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(routes::root_get)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-            .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-    );
+    let app = Router::new()
+        .route("/", get(routes::root_get))
+        .route("/convert", post(routes::convert_post))
+        .route("/download/{id}", get(routes::download_get))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
 
     let port = env::var("PORT").unwrap_or_else(|_| "3203".to_string());
 
