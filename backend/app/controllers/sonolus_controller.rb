75ea1c2c875a7602aa13fbf3aa55e3f3d81aeddb
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-12-04 21:16:11 +0900

    Update: Monkey patch for 0.7.4

--- backend/app/controllers/sonolus_controller.rb
+++ backend/app/controllers/sonolus_controller.rb
@@ -192,7 +192,7 @@ class SonolusController < ApplicationController
            }
   end
 
-  after_action { headers["Sonolus-Version"] = "0.7.3" }
+  after_action { headers["Sonolus-Version"] = "0.7.4" }
 
   around_action do |_controller, action|
     success = false
