728a18f2dc55d946c75d7ac5010975c39718da93
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-03-03 17:18:34 +0900

    Fix: Fix migration

--- backend/db/migrate/20230123102008_add_counter_culture_to_users.rb
+++ backend/db/migrate/20230123102008_add_counter_culture_to_users.rb
@@ -2,6 +2,6 @@ class AddCounterCultureToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :charts_count, :integer, default: 0, null: false
 
-    Chart.counter_culture_fix_counts
+    # Chart.counter_culture_fix_counts
   end
 end
