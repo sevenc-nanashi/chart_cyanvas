f88fecc3b4b666f78d4bc90d51d78da67a845f9b
Nanashi. <sevenc7c@sevenc7c.com>
2024-10-04 07:20:40 +0900

    Update: to Sonolus 0.8.6

--- backend/app/controllers/sonolus_controller.rb
+++ backend/app/controllers/sonolus_controller.rb
@@ -324,7 +324,7 @@ class SonolusController < ApplicationController
            }
   end
 
-  after_action { headers["Sonolus-Version"] = "0.8.5" }
+  after_action { headers["Sonolus-Version"] = "0.8.6" }
 
   around_action do |_controller, action|
     success = false
