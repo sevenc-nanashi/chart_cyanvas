d0afa3791476e15fcd42cc6d4a8335884df8a1c1
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-01-06 09:21:06 +0900

    Change: Use xterm for output

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -21,6 +21,8 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.active_support.to_time_preserves_timezone = :zone
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
