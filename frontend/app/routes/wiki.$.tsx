03d8fc66d0a13f12df5f5cbfb6eaa41bccc3f92d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-06-26 10:14:43 +0900

    fix: fix redirection

--- frontend/app/routes/wiki.$.tsx
+++ frontend/app/routes/wiki.$.tsx
@@ -1,14 +1,16 @@
 import type { LoaderFunction } from "@remix-run/node";
+import { host } from "~/lib/config.server";
 import { detectLocale } from "~/lib/i18n.server.ts";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const locale = await detectLocale(request);
 
-  const [beforeWiki, afterWiki] = request.url.split("/wiki/", 2);
+  const url = new URL(request.url);
+  const [_beforeWiki, afterWiki] = url.pathname.split("/wiki/");
 
   if (afterWiki.startsWith(`${locale}/`)) {
-    return Response.redirect(`${beforeWiki}/wiki/${afterWiki}`, 308);
+    return Response.redirect(`${host}/wiki/${afterWiki}`, 308);
   } else {
-    return Response.redirect(`${beforeWiki}/wiki/${locale}/${afterWiki}`, 308);
+    return Response.redirect(`${host}/wiki/${locale}/${afterWiki}`, 308);
   }
 };
