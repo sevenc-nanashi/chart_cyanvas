dc0e7042e3a1f67e45023f569fa51a61de2ce08a
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-08-02 11:20:26 +0900

    perf: use hiredis

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -6,6 +6,11 @@ logger =
   ActiveSupport::TaggedLogging.new(
     Console::Compatible::Logger.new("Sidekiq", Console.logger.output)
   )
+
+if Rails.env.production?
+  logger.level = :info
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch("REDIS_URL", nil), network_timeout: 10 }
   config.logger = logger
