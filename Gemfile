f8c8f99e35adb135d172397f079aeec45c13ef4b
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-08-02 12:07:26 +0900

    fix: fix installation

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 # Dirty hack to mark as root
 
 eval File.read("./backend/Gemfile")
-
-gem "opentelemetry-exporter-otlp", "~> 0.30.0"
--- backend/config/initializers/opentelemetry.rb
+++ backend/config/initializers/opentelemetry.rb
@@ -2,7 +2,7 @@
 require "console/compatible/logger"
 require "opentelemetry/sdk"
 require "opentelemetry/instrumentation/all"
-require "opentelemetry-exporter-otlp"
+require "opentelemetry/exporter/otlp"
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = "backend"
