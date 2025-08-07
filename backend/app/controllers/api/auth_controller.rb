3e837275f9c482be8963301ff7f9ddc93426ae06
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-08-07 21:37:56 +0900

    fix: fix host setting

--- backend/app/controllers/api/auth_controller.rb
+++ backend/app/controllers/api/auth_controller.rb
@@ -3,8 +3,16 @@ module Api
   class AuthController < FrontendController
     def start
       uuid = SecureRandom.uuid
+      host =
+        ENV.fetch("FINAL_HOST") do
+          if Rails.env.development?
+            request.host_with_port
+          else
+            raise "FINAL_HOST must be set in production"
+          end
+        end
       url =
-        "https://open.sonolus.com/external-login/#{request.host}/api/login/callback?uuid=#{uuid}"
+        "https://open.sonolus.com/external-login/#{host}/api/login/callback?uuid=#{uuid}"
       render json: { url:, uuid: }
     end
 
@@ -31,14 +39,16 @@ module Api
         render json: { error: "Invalid type" }, status: :unauthorized
         return
       end
-      if !Rails.env.development? && !params[:url].ends_with?(
-                 "//#{request.host}/api/login/callback?uuid=#{params[:uuid]}"
-               )
-          logger.warn "Invalid url: #{params[:url]}"
-          render json: { error: "Invalid url" }, status: :unauthorized
-          return
-        end
-      unless params[:time] && (Time.now.to_i - (params[:time] / 1000)) < 1.minute
+      if !Rails.env.development? &&
+           !params[:url].ends_with?(
+             "//#{request.host}/api/login/callback?uuid=#{params[:uuid]}"
+           )
+        logger.warn "Invalid url: #{params[:url]}"
+        render json: { error: "Invalid url" }, status: :unauthorized
+        return
+      end
+      unless params[:time] &&
+               (Time.now.to_i - (params[:time] / 1000)) < 1.minute
         render json: { error: "Expired time" }, status: :unauthorized
         return
       end
@@ -55,7 +65,11 @@ module Api
     def status
       uuid = params[:uuid]
       unless uuid
-        render json: { code: "not_found", message: "Not Found" }, status: :not_found
+        render json: {
+                 code: "not_found",
+                 message: "Not Found"
+               },
+               status: :not_found
         return
       end
 
@@ -65,7 +79,11 @@ module Api
           session[:user_id] = status.to_i
           render json: { code: "ok" }
         else
-          render json: { code: "not_found", message: "Not Found" }, status: :not_found
+          render json: {
+                   code: "not_found",
+                   message: "Not Found"
+                 },
+                 status: :not_found
         end
       end
     end
